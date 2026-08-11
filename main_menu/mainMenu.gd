68450beca6a5cbf7aab8ca39e439e4a8a6625e1d
AdrianRiegel <adrianriegel10@gmail.com>
2026-08-11 20:51:29 -0300

    Vicen aqui, o cara esqueceu de salvar o arquivo

--- main_menu/mainMenu.gd
+++ main_menu/mainMenu.gd
@@ -1,38 +1,38 @@
 extends Control
 
 # Caminhos para as cenas atualizados conforme a sua árvore de arquivos
-const CENA_NIVEL_INICIAL = "res://fase_1/fase_1.tscn"
-const CENA_OPCOES = "res://options/options.tscn"
+const START_SCENE = "res://fase_1/fase_1.tscn"
+const OPTION_SCENE = "res://options/options.tscn"
 
-@onready var btn_iniciar = $MenuContainer/BtnIniciar
-@onready var btn_continuar = $MenuContainer/BtnContinuar
-@onready var btn_opcoes = $MenuContainer/BtnOpcoes
-@onready var btn_sair = $MenuContainer/BtnSair
+@onready var btn_start = $MenuContainer/BtnStart
+@onready var btn_continue = $MenuContainer/BtnContinue
+@onready var btn_options = $MenuContainer/BtnOptions
+@onready var btn_exit = $MenuContainer/BtnExit
 
 func _ready():
 	# Conecta os botões aos seus respectivos métodos usando a sintaxe de Sinais do Godot 4
-	btn_iniciar.pressed.connect(_on_btn_iniciar_pressed)
-	btn_continuar.pressed.connect(_on_btn_continuar_pressed)
-	btn_opcoes.pressed.connect(_on_btn_opcoes_pressed)
-	btn_sair.pressed.connect(_on_btn_sair_pressed)
+	btn_start.pressed.connect(_on_btn_start_pressed)
+	btn_continue.pressed.connect(_on_btn_continue_pressed)
+	btn_options.pressed.connect(_on_btn_options_pressed)
+	btn_exit.pressed.connect(_on_btn_exit_pressed)
 	
 	# Foca no primeiro botão automaticamente para facilitar o uso do teclado
-	btn_iniciar.grab_focus()
+	btn_start.grab_focus()
 
-func _on_btn_iniciar_pressed():
+func _on_btn_start_pressed():
 	# Carrega a primeira fase do jogo
-	get_tree().change_scene_to_file(CENA_NIVEL_INICIAL)
+	get_tree().change_scene_to_file(START_SCENE)
 
-func _on_btn_continuar_pressed():
+func _on_btn_continue_pressed():
 	# Aqui entrará a lógica da Godot FileAccess / ConfigFile API 
 	# para carregar o progresso salvo localmente
 	print("Carregando o save state...")
 	pass
 
-func _on_btn_opcoes_pressed():
+func _on_btn_options_pressed():
 	# Transição para a interface estruturada de opções
-	get_tree().change_scene_to_file(CENA_OPCOES)
+	get_tree().change_scene_to_file(OPTION_SCENE)
 
-func _on_btn_sair_pressed():
+func _on_btn_exit_pressed():
 	# Encerra o executável final nativo do jogo
 	get_tree().quit()
