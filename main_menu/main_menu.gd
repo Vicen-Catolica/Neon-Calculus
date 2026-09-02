34111e7360e11ae29e2cb5bfcde1b3cdf865fef8
VICENZO LUIGGI MORI <vicenzo.mori@catolicasc.edu.br>
2026-09-02 20:21:19 -0300

    Changes to make button an static color, changing to another static color on hover/focus

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,9 +1,7 @@
 extends Control
 
-# Carrega o arquivo compilado diretamente
 const NEON_FONT = preload("res://assets/fonts/Orbitron-Bold.ttf")
 
-# Caminhos para as cenas atualizados conforme a sua árvore de arquivos
 @export_file("*.tscn") var start_scene: String = "res://fase_1/fase_1.tscn"
 @export_file("*.tscn") var option_scene: String = "res://options/options.tscn"
 
@@ -17,25 +15,31 @@ func _ready() -> void:
 	_setup_buttons()
 
 func _setup_buttons() -> void:
-	# Conexões de clique específicas
 	btn_start.pressed.connect(_on_start_pressed)
 	btn_continue.pressed.connect(_on_continue_pressed)
 	btn_options.pressed.connect(_on_options_pressed)
 	btn_exit.pressed.connect(get_tree().quit)
 	
-	# Automação de foco e cor de texto ao passar o mouse
-	for child in menu_container.get_children():
-		if child is Button:
-			child.mouse_entered.connect(child.grab_focus)
-			child.mouse_exited.connect(child.release_focus)
-	
 	_setup_buttons_font()
+	_setup_buttons_gradient_text()
 
 func _setup_buttons_font() -> void:
 	for child in menu_container.get_children():
 		if child is Button:
 			child.add_theme_font_override("font", NEON_FONT)
 			child.add_theme_font_size_override("font_size", 24)
+
+func _setup_buttons_gradient_text() -> void:
+	var empty_style := StyleBoxEmpty.new()
+
+	for child in menu_container.get_children():
+		if child is Button:
+			for state in ["normal", "hover", "pressed", "focus", "disabled"]:
+				child.add_theme_stylebox_override(state, empty_style)
+			
+			child.add_theme_color_override("font_color", Color("#00fbfc"))
+			child.add_theme_color_override("font_hover_color", Color("#82ff53ff"))
+			child.add_theme_color_override("font_focus_color", Color("#82ff53ff"))
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file(start_scene)
